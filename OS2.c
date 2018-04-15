7bea2d6cc2346db2f31b8abcf7a7b3e23dac10fc
sourabh kadyan <38257054+sourabhkadyan@users.noreply.github.com>
2018-04-15 21:05:35 +0530

    Update OS2.c

--- OS2.c
+++ OS2.c
@@ -1,71 +1,37 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
 int main()
 {
-	int m;
-	int reference_string[10],page_faults = m,n,s,pages,frames,i;
-	
-	printf("\nEnter Total number of pages:\n"):
-	scanf("%d",&pages);
-	
-	printf("\nEnter values of Reference string:\n");
-	for(m=0;m<pages;m++)
-	{
-		reference_string[m] = printRandoms(0,30,1);
-	}
-	for(m=0;m<pages;m++)
-	{
-		printf("Values No. [%d] Reference String %d:\n",m,reference_string[m]);
-	}
-
-	printf("\nEnter Total Number Of Frames:\t");
-	scanf("%d",&frames);
-
-	int temp[frames];
-
-	for(m=0;m<frames;m++)
-	{
-		temp[m]=-1;
-	}
-	printf("ref String\t\t page frames\n");
-	for(m=0;m<pages;m++)
-	{
-		s=0;
-
-		for(n=0;n<frames;n++)
-	{
-		if(reference_string[m]==temp[n])
-		{
-			s++;
-			page_faults--;
-		}
-	}
-	page_faults++;
-	if((page_faults<=frames)&&(s==0))
-	{
-		temp[m]=reference_string[m];
-	}
-	else if(s==0)
-	{
-		temp[(page_faults -1)%frames]=reference_string[m];
-	}
-	printf("\n");
-	for(n=0;n<frames;n++)
-	{
-		printf("%d\t",temp[n]);
-	}
-	}
-	printf("\n\nTotal page fault:\t%d\n",page_faults);
-	return 0;
+int i,j,n,a[50],frame[10],no,k,avail,count=0;
+            printf("\n ENTER THE NUMBER OF PAGES:\n");
+scanf("%d",&n);
+            printf("\n ENTER THE PAGE NUMBER :\n");
+            for(i=1;i<=n;i++)
+            scanf("%d",&a[i]);
+            printf("\n ENTER THE NUMBER OF FRAMES :");
+            scanf("%d",&no);
+for(i=0;i<no;i++)
+            frame[i]= -1;
+                        j=0;
+                        printf("\tref string\t page frames\n");
+for(i=1;i<=n;i++)
+                        {
+                                    printf("%d\t\t",a[i]);
+                                    avail=0;
+                                    for(k=0;k<no;k++)
+if(frame[k]==a[i])
+                                                avail=1;
+                                    if (avail==0)
+                                    {
+                                                frame[j]=a[i];
+                                                j=(j+1)%no;
+                                                count++;
+                                                for(k=0;k<no;k++)
+                                                printf("%d\t",frame[k]);
 }
-int printRandoms(int lower,int upper,int count)
-{
-	int i;
-	for(i=0;i<count;i++)
-	{
-		int num = (rand()+lower)%(upper+1);
-		return num;
-	}
-	return 0;
+                                    printf("\n");
 }
+                        printf("Page Fault Is %d",count);
+                        return 0;
+}
+
+
